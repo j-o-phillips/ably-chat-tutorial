3e4213c89f9ba3d082a0c808b31bb6cb87ac15a3
j-o-phillips <jphillips105@googlemail.com>
2024-04-18 12:40:31 +0200

    css name change

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -4,9 +4,9 @@ import * as Ably from "ably";
 import { AblyProvider, ChannelProvider } from "ably/react";
 import ChatBox from "./ChatBox";
 
-export default function Chat() {
-  const client = new Ably.Realtime({ authUrl: "/api", authMethod: "POST" });
+const client = new Ably.Realtime({ authUrl: "/api", authMethod: "POST" });
 
+export default function Chat() {
   return (
     <AblyProvider client={client}>
       <ChannelProvider channelName="chat-demo">
