46bb1280c30421de4c9e498aa45c40d4f913596f
j-o-phillips <jphillips105@googlemail.com>
2024-04-18 12:37:31 +0200

    css name change

--- app/api/route.js
+++ app/api/route.js
@@ -1,6 +1,6 @@
 import * as Ably from "ably";
 
-export async function GET(request) {
+export async function POST(request) {
   const client = new Ably.Realtime(process.env.ABLY_API_KEY);
   const tokenRequestData = await client.auth.createTokenRequest({
     clientId: "ably-chat-tutorial",
